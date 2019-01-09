9f222bc1110b4264f9eb731458c592fad3f5b91c
gabrielcoder <gabrielcoder247@gmail.com>
2019-01-09 15:44:18 +0300

    query database from views

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Tweet
 
 # Create your views here.
+
+def tweet_detail_view(request, id=1):
+    return render(request,"tweets/detail_view.html",{})
+
+def tweet_list_view(request, id=1):
+    return render(request,"tweets/list_view.html",{})    
