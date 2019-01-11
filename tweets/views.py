097375ac4a9409efde0a287a8b261a83d14fed1a
gabrielcoder <gabrielcoder247@gmail.com>
2019-01-12 01:16:34 +0300

    create tweeting from homepage

--- tweets/views.py
+++ tweets/views.py
@@ -58,7 +58,8 @@ class TweetListView(LoginRequiredMixin,ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(TweetListView, self).get_context_data(*args, **kwargs)
-
+        context['create_form'] = TweetModelForm()
+        context['create_url'] = reverse_lazy("tweet:create")
         # context["another_list"] = Tweet.objects.all()
         # print(context)
         return context
