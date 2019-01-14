d4797c70e5e2f3bab490a38da724b5ca19d843ba
gabrielcoder <gabrielcoder247@gmail.com>
2019-01-14 11:32:06 +0300

    jquery form

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,5 +1,6 @@
 
 from rest_framework import generics
+from django.db.models import Q
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 
@@ -8,8 +9,17 @@ class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
     # pagination_class = StandardResultsPagination
 
-    def get_queryset(self):
-        return Tweet.objects.all()
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        print(self.request.GET)
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                    Q(content__icontains=query) |
+                    Q(user_id__username__icontains=query)
+                    )
+        return qs    
 
     # def get_serializer_context(self, *args, **kwargs):
     #     context = super(TweetListAPIView, self).get_serializer_context(*args, **kwargs)
