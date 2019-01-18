922661982ddda4daac0a6fdcaf66b9b2119c5ac7
gabrielcoder <gabrielcoder247@gmail.com>
2019-01-19 02:38:43 +0300

    user tweet api endpoint

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -1,5 +1,4 @@
 """tweetme URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
 Examples:
@@ -13,22 +12,27 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from django.contrib import admin
+
 from django.conf import settings
 from django.conf.urls.static import static
-from tweets.views import TweetListView
+
 from hashtags.views import HashTagView
+from tweets.views import TweetListView
 from .views import home
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$',TweetListView.as_view(),name='home'),
+    url(r'^admin/', admin.site.urls), #admin/
+    url(r'^$', TweetListView.as_view(), name='home'), #/
     url(r'^tags/(?P<hashtag>.*)/$', HashTagView.as_view(), name='hashtag'),
+    url(r'^tweet/', include('tweets.urls', namespace='tweet')),
+
+    url(r'^api/tweet/', include('tweets.api.urls', namespace='tweet-api')),
+    url(r'^api/', include('accounts.api.urls', namespace='profiles-api')),
     url(r'^', include('accounts.urls', namespace='profiles')),
-    url(r'^tweet/', include('tweets.urls', namespace="tweet")),
-    url(r'^api/tweet/', include('tweets.api.urls', namespace="tweet-api")),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
